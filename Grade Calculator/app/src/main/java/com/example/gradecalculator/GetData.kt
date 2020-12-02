c3241535697dbf514065a811c949272c8c21b6b0
Alexander Johnson <75290142+atjohnson@users.noreply.github.com>
2020-12-01 22:55:15 -0500

    Added text and button to main activity

---
+++ Grade Calculator/app/src/main/java/com/example/gradecalculator/GetData.kt
@@ -0,0 +1,11 @@
+package com.example.gradecalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GetData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_data)
+    }
+}
